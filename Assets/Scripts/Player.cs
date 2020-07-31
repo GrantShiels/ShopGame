84f7aaba60b6982b8f18a70649387c5cfa6080ef
Grant Shiels <grant.shiels@btinternet.com>
2020-08-01 00:31:43 +0100

    Added player movment script
    Script is basic movment right now

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    //Player RigidBody
+    Rigidbody2D playerBody;
+
+    //Float values for both the verticall and horizontal directions
+    float horizontal;
+    float vertical;
+
+    //move speed
+    public float moveSpeed = 20.0f;
+
+
+
+    void Start()
+    {
+        //Get RigidBody and allocate it to playerBody
+        playerBody = GetComponent<Rigidbody2D>();
+    }
+
+
+    void Update()
+    {
+        horizontal = Input.GetAxisRaw("Horizontal");
+        vertical = Input.GetAxisRaw("Vertical");   
+    }
+
+
+    private void FixedUpdate() 
+    {
+        //set the body velocity
+        playerBody.velocity = new Vector2(horizontal * moveSpeed, vertical * moveSpeed);
+        
+    }
+}
