cc6b2e94d95afaf6044e51625f51f906e9e19427
Grant Shiels <grant.shiels@btinternet.com>
2020-08-01 13:05:07 +0100

    Created PC movment script

---
+++ Assets/Scripts/PlayerMovment.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Class that will be used to control how the PC moves
+public class PlayerMovment : MonoBehaviour
+{
+    
+    //The speed that the PC can move
+    public float moveSpeed = 5f;
+
+    //the rigid body for the PC
+    public Rigidbody2D playerRB;
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
