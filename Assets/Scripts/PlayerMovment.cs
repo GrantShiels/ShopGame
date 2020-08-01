8f5fc2e366a4008b00f3bce20c9950724821e9fd
Grant Shiels <grant.shiels@btinternet.com>
2020-08-01 13:10:37 +0100

    Updated PlayerMovment Script
    PC can move

--- Assets/Scripts/PlayerMovment.cs
+++ Assets/Scripts/PlayerMovment.cs
@@ -12,9 +12,24 @@ public class PlayerMovment : MonoBehaviour
     //the rigid body for the PC
     public Rigidbody2D playerRB;
 
+    //movment gets both the x and y to determine which move direction
+    Vector2 movment;
+
     // Update is called once per frame
+    //Will handle input
     void Update()
     {
+        //get input from keys and add them to the Vector2
+        movment.x = Input.GetAxisRaw("Horizontal");
+        movment.y = Input.GetAxisRaw("Vertical");
+    }
+
+    //fixed update time, not connected to frames. 
+    //Will handle movment
+    void FixedUpdate() 
+    {
+
+        playerRB.MovePosition(playerRB.position + movment * moveSpeed * Time.fixedDeltaTime);
         
     }
 }
