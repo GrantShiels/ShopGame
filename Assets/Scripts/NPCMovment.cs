ba4630c69045af4b673f81b9bdeef55b58fef247
Grant Shiels <grant.shiels@btinternet.com>
2020-08-06 15:38:30 +0100

    Created NPC movment Script and added Variables

---
+++ Assets/Scripts/NPCMovment.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+
+
+    //NPC RigidBody
+    Rigidbody2D NPCBody;
+
+    //Float values for both the verticall and horizontal directions
+    float horizontal;
+    float vertical;
+
+    //move speed
+    public float moveSpeed = 10.0f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        NPCBody = GetComponent<Rigidbody2D>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
