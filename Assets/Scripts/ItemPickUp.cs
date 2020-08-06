5d303307ef858aeb24f3ec0180e2f8f55366998a
Grant Shiels <grant.shiels@btinternet.com>
2020-08-06 17:01:20 +0100

    removed old Script file

--- Assets/Scripts/ItemPickUp.cs
+++ Assets/Scripts/ItemPickUp.cs
@@ -2,8 +2,16 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+//Class that is used to allow the user to pick up the shop products
 public class ItemPickUp : MonoBehaviour
 {
+    public GameObject Item;
+
+    //When the player enters the items collison
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+       //PlayerMovment player = Collision. 
+    }
     // Start is called before the first frame update
     void Start()
     {
